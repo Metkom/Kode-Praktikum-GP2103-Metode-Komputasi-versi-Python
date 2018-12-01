0f136c290829c293d9860edb9354495c35d1e50f
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-12-02 00:35:48 +0700

    Add files via upload

---
+++ pm3a.py
@@ -0,0 +1,13 @@
+"""
+Modul Praktikum GP2103 Metode Komputasi,
+Program Studi Teknik Geofisika, Universitas Pertamina
+Oleh: Mohammad Heriyanto
+Website: https://osf.io/h6d5w/
+Link Kode: https://github.com/Metkom/Kode-Praktikum-GP2103-Metode-Komputasi-versi-Python
+DOI: 10.17605/OSF.IO/H6D5W
+@ Desember 2018
+"""
+
+a = 4
+if a % 2 == 0:
+    print('a adalah bilangan genap')
