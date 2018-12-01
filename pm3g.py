8a3c22385e39cb3edb4d1e6e8808ffbe80843e73
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-12-02 00:48:07 +0700

    Add files via upload

---
+++ pm3g.py
@@ -0,0 +1,18 @@
+"""
+Modul Praktikum GP2103 Metode Komputasi,
+Program Studi Teknik Geofisika, Universitas Pertamina
+Oleh: Mohammad Heriyanto
+Website: https://osf.io/h6d5w/
+Link Kode: https://github.com/Metkom/Kode-Praktikum-GP2103-Metode-Komputasi-versi-Python
+DOI: 10.17605/OSF.IO/H6D5W
+@ Desember 2018
+"""
+
+var = 1
+while var == 1:
+    num = input("Enter a number  :\n")
+    if num == 'x' or num == 'stop':
+        break
+        print("Good bye!")
+    else:
+        print("You entered: ", num)
