fb7cef51d4e957621b4e078725660c1e52e0e9ef
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-12-01 23:47:37 +0700

    Add files via upload

---
+++ pm1e.py
@@ -0,0 +1,21 @@
+"""
+Modul Praktikum GP2103 Metode Komputasi,
+Program Studi Teknik Geofisika, Universitas Pertamina
+Oleh: Mohammad Heriyanto
+Website: https://osf.io/h6d5w/
+Link Kode: https://github.com/Metkom/Kode-Praktikum-GP2103-Metode-Komputasi-versi-Python
+DOI: 10.17605/OSF.IO/H6D5W
+@ Desember 2018
+"""
+
+a = (1, 2, 3)  # tuples
+b = (3, 4, 5)  # tuples
+
+c = [1, 2, 3]  # list
+d = [3, 4, 5]  # list
+
+print('a[0]+c[0]: ', a[0]+c[0])
+print('b[-1]-d[-1]: ', b[-1]-d[-1])
+
+print('a[1]*c[1]: ', a[1]*c[1])
+print('b[1]/d[1]: ', b[1]/d[1])
