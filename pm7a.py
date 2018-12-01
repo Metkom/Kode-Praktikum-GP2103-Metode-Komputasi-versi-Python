440cc66ad62b09b0313235ee9af0d4f3579882cf
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-12-02 02:53:23 +0700

    Add files via upload

---
+++ pm7a.py
@@ -0,0 +1,17 @@
+"""
+Modul Praktikum GP2103 Metode Komputasi,
+Program Studi Teknik Geofisika, Universitas Pertamina
+Oleh: Mohammad Heriyanto
+Website: https://osf.io/h6d5w/
+Link Kode: https://github.com/Metkom/Kode-Praktikum-GP2103-Metode-Komputasi-versi-Python
+DOI: 10.17605/OSF.IO/H6D5W
+@ Desember 2018
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 2 * np.pi, np.pi / 100)
+y = np.sin(x)
+plt.plot(x, y)
+plt.show()
