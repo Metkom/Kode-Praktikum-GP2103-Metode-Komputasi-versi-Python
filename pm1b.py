d0b3b2f3e87f0a256144ed432b7a8ce6cc7cc5d3
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-12-01 23:39:45 +0700

    Add files via upload

---
+++ pm1b.py
@@ -0,0 +1,22 @@
+"""
+Modul Praktikum GP2103 Metode Komputasi,
+Program Studi Teknik Geofisika, Universitas Pertamina
+Oleh: Mohammad Heriyanto
+Website: https://osf.io/h6d5w/
+Link Kode: https://github.com/Metkom/Kode-Praktikum-GP2103-Metode-Komputasi-versi-Python
+DOI: 10.17605/OSF.IO/H6D5W
+@ Desember 2018
+"""
+
+a = 5
+b = 2.0
+c = 2 + 1j
+
+print('Penjumlahan antara a dan b: ', a + b)
+print('Pengurangan antara a dan b: ', a - b)
+print('Perkalian antara a dan b: ', a * b)
+print('Pembagian antara a dan b: ', a / b)
+print('Pembagian antara a dan b dengan pembulatan: ', int(b / a))
+print("Sisa pembagian (modulo) antara a dan b: ", a % b)
+print('Perpangkatan antara a dan b: ', a ** b)
+print('Perpangkatan antara a dan b: ', pow(a, b))
