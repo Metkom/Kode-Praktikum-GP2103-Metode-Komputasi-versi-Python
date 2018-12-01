c95ec27608719bf1aa887073a98be7343685e72e
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-12-02 02:38:40 +0700

    Add files via upload

---
+++ pm6a.py
@@ -0,0 +1,12 @@
+"""
+Modul Praktikum GP2103 Metode Komputasi,
+Program Studi Teknik Geofisika, Universitas Pertamina
+Oleh: Mohammad Heriyanto
+Website: https://osf.io/h6d5w/
+Link Kode: https://github.com/Metkom/Kode-Praktikum-GP2103-Metode-Komputasi-versi-Python
+DOI: 10.17605/OSF.IO/H6D5W
+@ Desember 2018
+"""
+
+g = lambda x: 3 * x + 1
+print(g(2))
