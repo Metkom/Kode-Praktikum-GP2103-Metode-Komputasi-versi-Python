6edbea709c5bac239b0b01391d76753de8531f1b
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-12-02 02:40:19 +0700

    Add files via upload

---
+++ pm6b.py
@@ -0,0 +1,18 @@
+"""
+Modul Praktikum GP2103 Metode Komputasi,
+Program Studi Teknik Geofisika, Universitas Pertamina
+Oleh: Mohammad Heriyanto
+Website: https://osf.io/h6d5w/
+Link Kode: https://github.com/Metkom/Kode-Praktikum-GP2103-Metode-Komputasi-versi-Python
+DOI: 10.17605/OSF.IO/H6D5W
+@ Desember 2018
+"""
+
+
+# fungsi pendukung
+def salam():
+    print("Selamat Datang di Metkom")
+
+
+# fungsi utama
+salam()
