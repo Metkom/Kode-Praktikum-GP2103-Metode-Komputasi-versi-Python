d2580ac4985197c0c96ada542a8136782dfabc04
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-12-01 23:26:30 +0700

    Add files via upload

---
+++ pm1a.py
@@ -0,0 +1,22 @@
+"""
+Modul Praktikum GP2103 Metode Komputasi,
+Program Studi Teknik Geofisika, Universitas Pertamina
+Oleh: Mohammad Heriyanto
+Website: https://osf.io/h6d5w/
+Link Kode: https://github.com/Metkom/Kode-Praktikum-GP2103-Metode-Komputasi-versi-Python
+DOI: 10.17605/OSF.IO/H6D5W
+@ Desember 2018
+"""
+
+s = '''Pycharm: "Selamat Datang di Praktikum Metkom"'''
+a = 5  # int
+b = 2.0  # float
+c = 2 + 1j  # complex
+
+print('String: ', s)
+print('Int: ', a)
+print('Float: ', b)
+print('Bagian Complex: ', c)
+print('Bagian Real: ', c.real)
+print('Bagian Imag: ', c.imag)
+print(type(c))
