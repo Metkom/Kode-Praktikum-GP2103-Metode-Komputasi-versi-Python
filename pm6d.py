fff521a96f94115bfe6bbdface31d4d33d311955
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-12-02 02:43:42 +0700

    Add files via upload

---
+++ pm6d.py
@@ -0,0 +1,15 @@
+"""
+Modul Praktikum GP2103 Metode Komputasi,
+Program Studi Teknik Geofisika, Universitas Pertamina
+Oleh: Mohammad Heriyanto
+Website: https://osf.io/h6d5w/
+Link Kode: https://github.com/Metkom/Kode-Praktikum-GP2103-Metode-Komputasi-versi-Python
+DOI: 10.17605/OSF.IO/H6D5W
+@ Desember 2018
+"""
+
+from pm6c import v
+
+r = int(input('Input nilai r : '))
+vol_bola = v(r)
+print('Volume Bola dengan r : %d adalah ' % v(r), vol_bola)
