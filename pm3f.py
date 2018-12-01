c0094a607213a3dad0c4c4ff0a135de8f196e9f4
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-12-02 00:46:44 +0700

    Add files via upload

---
+++ pm3f.py
@@ -0,0 +1,19 @@
+"""
+Modul Praktikum GP2103 Metode Komputasi,
+Program Studi Teknik Geofisika, Universitas Pertamina
+Oleh: Mohammad Heriyanto
+Website: https://osf.io/h6d5w/
+Link Kode: https://github.com/Metkom/Kode-Praktikum-GP2103-Metode-Komputasi-versi-Python
+DOI: 10.17605/OSF.IO/H6D5W
+@ Desember 2018
+"""
+
+for letter in 'Python':
+    if letter == 'h':
+        break
+    print('Current Letter :', letter)
+print(20*'=')
+for letter in 'Python':
+    if letter == 'h':
+        continue
+    print('Current Letter :', letter)
