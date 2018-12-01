a7497f306aeb6ee92516d906893a501cff7530ea
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-12-02 00:40:13 +0700

    Add files via upload

---
+++ pm3d.py
@@ -0,0 +1,14 @@
+"""
+Modul Praktikum GP2103 Metode Komputasi,
+Program Studi Teknik Geofisika, Universitas Pertamina
+Oleh: Mohammad Heriyanto
+Website: https://osf.io/h6d5w/
+Link Kode: https://github.com/Metkom/Kode-Praktikum-GP2103-Metode-Komputasi-versi-Python
+DOI: 10.17605/OSF.IO/H6D5W
+@ Desember 2018
+"""
+
+count = 1
+while count < 9:
+    print('The count is:', count)
+    count = count + 1
